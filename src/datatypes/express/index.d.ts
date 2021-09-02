aec1d0590719225cc1d86c70198a2e1eeff6093f
Kolawole Ojo <philliphopeojo@gmail.com>
2021-09-02 22:41:35 +0100

    Express Request datatype exended for functionality

---
+++ src/datatypes/express/index.d.ts
@@ -0,0 +1,8 @@
+declare namespace Express {
+    type ServiceSuccessfulResponse = import('../customIntegrations').ServiceSuccessfulResponse;
+    type ServiceFailedResponse = import('../customIntegrations').ServiceFailedResponse;
+
+    export interface Request {
+        payload: ServiceSuccessfulResponse | ServiceFailedResponse;
+    }
+}
