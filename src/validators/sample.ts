e1113558a77322cf1748c25efe8939a09ce6fcdb
Kolawole Ojo <philliphopeojo@gmail.com>
2021-09-06 02:32:27 +0100

    Validator completed

--- src/validators/sample.ts
+++ src/validators/sample.ts
@@ -1,14 +1,12 @@
 import Joi from 'joi';
 
-const createSchema = Joi.object({
+export const createSchema = Joi.object({
     id: Joi.string().min(1).max(Number.MAX_SAFE_INTEGER),
     param: Joi.string().required(),
     justValue: Joi.number(),
 });
 
-const editSchema = Joi.object({
+export const editSchema = Joi.object({
     id: Joi.string().min(1).max(Number.MAX_SAFE_INTEGER),
     param: Joi.string(),
 });
-
-export = { createSchema, editSchema };
